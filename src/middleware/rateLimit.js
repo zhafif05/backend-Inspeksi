93582567d02b0d217bc8c53ad86142ac660195f4
Muhammad Zhafif Rizqullah <zhafifriz05@gmail.com>
2026-07-09 16:27:12 +0700

    update-limit

--- src/middleware/rateLimit.js
+++ src/middleware/rateLimit.js
@@ -2,7 +2,7 @@ const rateLimit = require("express-rate-limit");
 
 const loginLimiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 menit
-  max: 9999,
+  max: 10,
   standardHeaders: true,
   legacyHeaders: false,
   message: {
